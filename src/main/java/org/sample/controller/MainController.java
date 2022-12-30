dc41955632f3b732abdef6837efb6eaa0f577a80
Jang <ahhhaaah@gmail.com>
2022-12-30 17:08:02 +0900

    feat: 프로젝트 세팅

---
+++ src/main/java/org/sample/controller/MainController.java
@@ -0,0 +1,23 @@
+package org.sample.controller;
+
+import lombok.extern.log4j.Log4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@Log4j
+public class MainController {
+
+    @RequestMapping(value = "/")
+    public ModelAndView index(){
+        log.info("index controller start!!");
+
+        ModelAndView mav = new ModelAndView();
+        mav.setViewName("index");
+        mav.addObject("title", "Spring Test");
+        mav.addObject("content", "테스트합시당");
+
+        return mav;
+    }
+}
